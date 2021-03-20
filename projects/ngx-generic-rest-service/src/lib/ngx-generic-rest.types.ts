b66d7bfcd44e764088f154831c14e85c4f79f66b
juanmesa2097 <juanmesa2097@gmail.com>
2021-03-20 00:41:27 -0500

    chore: add service configuration types

--- projects/ngx-generic-rest-service/src/lib/ngx-generic-rest.types.ts
+++ projects/ngx-generic-rest-service/src/lib/ngx-generic-rest.types.ts
@@ -12,11 +12,6 @@ export type ResultMessage = {
   errorMsg?: string;
 };
 
-type _httpHeaders = HttpHeaders | { [header: string]: string | string[] };
-type _httpParams = HttpParams | { [param: string]: string | string[] };
-type _observe = 'body' | 'events' | 'response';
-type _responseType = 'arraybuffer' | 'blob' | 'json' | 'text';
-
 export type HttpRequestOptions = {
   headers?: _httpHeaders;
   params?: _httpParams;
@@ -26,13 +21,19 @@ export type HttpRequestOptions = {
   withCredentials?: boolean;
 };
 
+type _httpHeaders = HttpHeaders | { [header: string]: string | string[] };
+type _httpParams = HttpParams | { [param: string]: string | string[] };
+type _observe = 'body' | 'events' | 'response';
+type _responseType = 'arraybuffer' | 'blob' | 'json' | 'text';
+
 export type HttpOptions<T = any> = HttpRequestOptions & {
-  url?: string;
+  urlRewrite?: string;
   urlPostfix?: string;
-  mapResponseFn?: (res: any) => T;
+  mapFn?: (res: any) => T;
 };
 
-export type HttpGetOptions<T = any> = HttpOptions<T> & ResultMessage;
+export type HttpGetAllOptions<T = any> = HttpOptions<T> & ResultMessage;
+export type HttpGetSingleOptions<T = any> = HttpOptions<T> & ResultMessage;
 export type HttpAddOptions<T = any> = HttpOptions<T> & ResultMessage;
 export type HttpUpdateOptions<T = any> = HttpOptions<T> & {
   method?: 'PUT' | 'PATCH';
